e410ca4f9e73e83652ea34a679eef34fa830e3f8
Abdelhak Akermi <abdelhak@akermi.me>
2020-02-17 10:47:58 +0300

    test(plugin.spec.js): test multiple fields update

--- tests/plugin.spec.js
+++ tests/plugin.spec.js
@@ -10,6 +10,7 @@ test.after.always(after);
 
 test.serial('post save hook ', async t => {
 	const {client} = t.context;
+	await client.indices.refresh({index: 'user'});
 	const {body} = await client.search({
 		index: 'user',
 		body: {
@@ -21,9 +22,29 @@ test.serial('post save hook ', async t => {
 
 	t.is(body.hits.total.value, 1);
 });
+
+
 test.serial('post updateone hook ', async t => {
 	const {client} = t.context;
 	await User.updateOne({email: 'one@example.com'}, {name: 'Two'});
+	await client.indices.refresh({index: 'user'});
+	const {body} = await client.search({
+		index: 'user',
+		body: {
+			query: {
+				match_all:{}
+			}
+		}
+	});
+	const _doc = body.hits.hits[0]._source;
+	t.is(body.hits.hits.length, 1);
+	t.is(_doc.name, 'Two');
+});
+
+
+test.serial('post updateone hook with multiple field  ', async t => {
+	const {client} = t.context;
+	await User.updateOne({email: 'one@example.com'}, {name: 'Two', country: 'fr'});
 	await client.indices.refresh({index: 'user'});
 	const {body} = await client.search({
 		index: 'user',
